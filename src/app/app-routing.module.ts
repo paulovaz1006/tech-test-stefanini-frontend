51619c654c7622aef52bdcc02a2d0bf3c1e984c8
paulovaz1006 <pvaz1006@gmail.com>
2023-11-07 12:12:04 -0300

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './employees/employees.component';
+import { CreateComponent } from './employees/components/create/create.component';
+import { EditComponent } from './employees/components/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'employees',
+    pathMatch: 'full',
+  },
+  {
+    path: 'employees',
+    component: EmployeesComponent,
+  },
+  {
+    path: 'create-employee',
+    component: CreateComponent,
+  },
+  {
+    path: 'edit-employee/:id',
+    component: EditComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
